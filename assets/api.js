08a3d3373ca002cef7c5bdf59cd1fb2cd18f2965
Santiagoh19 <santi07hernandezs@gmail.com>
2025-11-12 16:55:15 -0500

    Update api.js

--- assets/api.js
+++ assets/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000", // cambia si usas otro puerto/host
+  baseURL: "https://backend-h2ah.onrender.com", // cambia si usas otro puerto/host
 });
 
 // agrega token si existe
@@ -11,3 +11,4 @@ api.interceptors.request.use((config) => {
   if (t) config.headers.Authorization = `Bearer ${t}`;
   return config;
 });
+
